4d2b59214e2a493d96135a97c6d9321b3b1d3c7a
wannabeing <wannabeing@naver.com>
2022-06-30 11:50:02 +0900

    for published

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Coins from "./routes/Coins";
 import Coin from "./routes/Coin";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path="/:id">
           <Coin />
         </Route>
-        <Route path="/">
+        <Route path={process.env.PUBLIC_URL + "/"}>
           <Coins />
         </Route>
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 export default Router;
